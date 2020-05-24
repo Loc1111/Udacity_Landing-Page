f23b2ac5d371ebd89cd4191acac1541d433ff4c6
Loc1111 <43489503+Loc1111@users.noreply.github.com>
2020-05-24 21:25:56 +0700

    Update app.js

--- app.js
+++ app.js
@@ -1,73 +1,17 @@
-/**
- * Build navigation.
- * Manipulating the DOM exercise.
- * Exercise programmatically builds navigation,
- * scrolls to anchors from navigation,
- * and highlights section in viewport upon scrolling.
- * 
- * Dependencies: None
- * 
- * JS Version: ES2015/ES6
- * 
- * JS Standard: ESlint
- * 
-*/
-
-/**
- * Define Global Variables
- * 
-*/
-
-
-/**
- * End Global Variables
- * Start Helper Functions
- * 
-*/
-
-
-
-/**
- * End Helper Functions
- * Begin Main Functions
- * 
-*/
-
-// build the nav
-
-
-// Add class 'active' to section when near top of viewport
-
-
-// Scroll to anchor ID using scrollTO event
-
-
-/**
- * End Main Functions
- * Begin Events
- * 
-*/
-
-// Build a global variable 
+// Build a global variable
 const navbarMenu = document.getElementById('navbar_list');
-
-function createMenu(){
-  var sections = document.getElementsByTagName('SECTION');
+const sections = document.querySelectorAll('section');
   
-  let i;
-  for( i = 0; i < sections.length; i++){
-    var a = document.createElement('A');
-    var nodeName = sections[i].getAttribute('data-nav');
+  var i;
+  for (i = 0; i < sections.length; i++){
+    var navItem = document.createElement('LI');
+    var elementName = sections[i].getAttribute('data-nav');
     var link = sections[i].textContent;
-    a.appendChild(nodeName);
-    a.appendChild(link);
-  }
-    var node = document.createElement('LI');     
-    node.appendChild(a);
-    node.setAttribute('style','.navbar_menu');
-    navbarMenu.appendChild(node);
-  }
-  document.body.appendChild(navbarMenu); 
-  navbarMenu.setAttribute('style', '.menu_link');
-
- createMenu();
+    navItem.append(elementName);
+    navItem.append(link);
+    navItem.setAttribute('style', 'navbar_menu li');
+  };
+  navbarMenu.appendChild(navItem);
+  navbarMenu.setAttribute('style', 'menu_link');
+  navbarMenu.setAttribute('style', 'menu_link:hover');
+  document.body.appendChild(navbarMenu);
