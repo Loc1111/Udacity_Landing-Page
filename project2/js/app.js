72330c0daed7dfd7a871e59539fe449c8c79cc5b
Loc D <43489503+Loc1111@users.noreply.github.com>
2020-05-29 11:00:47 +0700

    Update app.js

--- project2/js/app.js
+++ project2/js/app.js
@@ -2,14 +2,14 @@ const navbarMenu = document.getElementById("navbar__list");
 const sections = document.querySelectorAll("section");
 
 function createNav(){
-  for (let section of sections){
-    let item = document.createElement("LI");
-    item.className = section.getAttribute("navbar__menu li");
-    item.title = section.getAttribute("data-nav");
-    item.textContent = section.textContent;
-    navbarMenu.appendChild(item);
-  };
-  document.body.appendChild(navbarMenu);
-}
+  for (let item of sections){
+    let section = document.createElement("li");
+    section.className = "menu__link";
+    section.dataset.nav = item.id;
+    section.innerText = item.dataset.nav;
+    navbarMenu.appendChild(section);
+  };  
+};
 createNav();
+
 
