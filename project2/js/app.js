d7d1ac47e8cbce4e65d5f8c5478dfa968f825f0c
Loc D <43489503+Loc1111@users.noreply.github.com>
2020-06-02 16:52:13 +0700

    Update app.js

--- project2/js/app.js
+++ project2/js/app.js
@@ -1,6 +1,25 @@
+/**
+ * 
+ * Manipulating the DOM exercise.
+ * Exercise programmatically builds navigation,
+ * scrolls to anchors from navigation,
+ * and highlights section in viewport upon scrolling.
+ * 
+ * Dependencies: None
+ * 
+ * JS Version: ES2015/ES6
+ * 
+ * JS Standard: ESlint
+ * 
+*/
+//set the starting time  to measure the speed of the code
+
+//select a ul tag in nav
 const navbarMenu = document.getElementById("navbar__list");
+//select sections
 const sections = document.querySelectorAll("section");
 
+//build a nav
 function createNav(){
   for (let item of sections){
     let section = document.createElement("li");
@@ -12,4 +31,20 @@ function createNav(){
 };
 createNav();
 
+// make section active
+fucntion makeActive(){
+  for (const section of sections){
+    const box = section.getBoundingClientRect();
+    if(box.top <= 150 && box.bottom => 150){
+      section.classList.add('your-active-class');
+      section.style.cssText = 'background-color: green';
+    }else{
+      section.classList.remove('your-active-class');
+      section.style.cssText = 'background-color: blue';
+    }
+  }
+}
 
+document.addEventListener('scroll', function(){
+  makeActive()
+});
