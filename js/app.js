c5ee1fc20f79fa6ad3efb13b19a77e1741ccc913
Loc D <43489503+Loc1111@users.noreply.github.com>
2020-06-19 15:38:20 +0700

    Update app.js

--- js/app.js
+++ js/app.js
@@ -15,15 +15,15 @@
 //set the starting time  to measure the speed of the code
 
 //select a ul tag in nav
-const navbarMenu = document.getElementById("navbar__list");
+const navbarMenu = document.getElementById('navbar__list');
 //select sections
-const sections = document.querySelectorAll("section");
+const sections = document.querySelectorAll('section');
 
 //build a nav
 function createNav(){
   for (let item of sections){
-    let section = document.createElement("li");
-    section.className = "menu__link";
+    let section = document.createElement('li');
+    section.className = 'menu__link';
     section.dataset.nav = item.id;
     section.innerText = item.dataset.nav;
     navbarMenu.appendChild(section);
@@ -36,17 +36,20 @@ function makeActive(){
   for (const section of sections) {
     const box = section.getBoundingClientRect();
     if(box.top <= 150 && box.bottom >= 150){
-      const id = section.getAttribute("id");
-      document.querySelector(`.${id}`).classList.add("active");      
+      const section = document.createElement(section)
+      const id = section.getAttribute('id');
+      document.querySelector(`.${id}`).classList.add('active');      
       section.classList.add('your-active-class');      
     }else{
-      const id = section.getAttribute("id");
+      const section = document.createElement(section)
+      const id = section.getAttribute('id');
       document.querySelector(`.${id}`).classList.remove('active');
       section.classList.remove('your-active-class');      
     }
   }
 }
 
-document.addEventListener("scroll", function(){
+document.addEventListener('scroll', function(){
   makeActive()
 });
+
